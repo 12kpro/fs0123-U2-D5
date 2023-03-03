1d723dd4017fe2ec21e2be8107bd3a3c10e16d86
Mauro Simoni <mauro.simoni@gmail.com>
2023-03-03 16:57:16 +0100

    animazione m

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,9 +6,15 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
     window.addEventListener('scroll', function() { 
         scrollpos = window.scrollY;
-        console.log(scrollpos);
         scrollpos >= scrollChange 
                     ?  menu.classList.add("bg-white")
                     :  menu.classList.remove("bg-white")
     })
-    })
+
+    let mList = document.querySelectorAll("svg  path")
+    let anim = setInterval(() => {
+        let rand = Math.floor(Math.random() * mList.length)
+        let m = mList[rand]
+        m.style.opacity ? m.style.opacity = 1 : m.style.opacity = 0
+    }, 500);
+})
